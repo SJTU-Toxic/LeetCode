99279277507b58ad6a9e0c8879f117c4292223b6
XieChang <toxic.xc@sjtu.edu.cn>
2016-07-24 22:31:54 +0800

    Update P006_longestPalindromicSubstring.cpp

--- P006_longestPalindromicSubstring.cpp
+++ P006_longestPalindromicSubstring.cpp
@@ -38,3 +38,46 @@ public:
         return s.substr(pos, max);
     }
 };
+// Solution 2
+// time: O(n)
+// space: O(n)
+class Solution2 {
+public:
+    //pre-process
+    string process(const string& s) {
+        string res = "#";
+        for(int i = 0; i < s.size(); i++) {
+            res += s[i];
+            res += '#';
+        }
+        return res;
+    }
+    string longestPalindrome(string s) {
+        if(s.size() <= 1) return s;
+        string temp = process(s);
+        //the string is not longer than 1000, so the pre-processed string would not be longer than 2001
+        int p[2001];
+        for(int i = 0; i < temp.size(); i++) p[i] = 0;
+        int pos = 0;
+        int curMax = 0;
+        int max = 1, idx = 0;
+        for(int i = 0; i < temp.size(); i++) {
+            int mirror = 2 * pos - i;
+            if(curMax > i) {
+                p[i] = min(p[mirror], curMax - i);
+            }
+            while(temp[i - p[i] - 1] == temp[i + p[i] + 1] && (i - p[i] >= 0) && (i + p[i] < temp.size())) p[i]++;
+            if(p[i] + i > curMax) {
+                pos = i;
+                curMax = pos + p[i];
+                if(p[i] > max) {
+                    idx = i;
+                    max = p[i];
+                    // already reach the end of string
+                    if(max >= temp.size() - i) break;
+                }
+            }
+        }
+        return s.substr((idx - max) / 2, max);
+    }
+};
