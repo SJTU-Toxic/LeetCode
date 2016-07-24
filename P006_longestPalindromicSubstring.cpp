c22c76c538430d93f27f2dada2a13a8592afcd68
XieChang <toxic.xc@sjtu.edu.cn>
2016-07-24 18:00:56 +0800

    Update P006_longestPalindromicSubstring.cpp

--- P006_longestPalindromicSubstring.cpp
+++ P006_longestPalindromicSubstring.cpp
@@ -1,1 +1,40 @@
-
+// Solution 1 : brute force
+// time: O(n^2) space: O(1)
+class Solution1 {
+public:
+    string longestPalindrome(string s) {
+        int max = 1, pos = 0;
+        for(int i = 0; i <s.size(); i++) {
+            int j = i, k = i + 1;
+            int tmp, len;
+            while(j >= 0 && k < s.size()) {
+                if(s[j] == s[k]) {
+                    tmp = j;
+                    len = k - j + 1;
+                    if(len > max) {
+                        max = len;
+                        pos = tmp;
+                    }
+                    j--;
+                    k++;
+                }
+                else break;
+            }
+            j = i - 1, k = i + 1;
+            while(j >= 0 && k < s.size()) {
+                if(s[j] == s[k]) {
+                    tmp = j;
+                    len = k - j + 1;
+                    if(len > max) {
+                        max = len;
+                        pos = tmp;
+                    }
+                    j--;
+                    k++;
+                }
+                else break;
+            }
+        }
+        return s.substr(pos, max);
+    }
+};
